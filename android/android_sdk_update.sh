cc1e40fee7231b20d8a50dadd87415dd0aa23da7
sinlov <sinlovgmppt@gmail.com>
2017-06-24 18:28:27 +0800

    let android sdk update check env

--- android/android_sdk_update.sh
+++ android/android_sdk_update.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
-# this scirpt need set ANDROID_HOME
+checkFuncBack(){
+  if [ $? -eq 0 ]; then
+    echo "$1 checked"
+  else
+    echo "$1 check error exit"
+    exit 1
+  fi
+}
+
+echo "This script need set ANDROID_HOME and event be right!"
+
+check_android=$(echo $ANDROID_HOME)
+if [ ! -n "${check_android}" ]; then
+    echo "you have not set ANDROID_HOME"
+    exit 1
+else
+    echo "ANDROID_HOME:${check_android}"
+fi
+
+which java
+checkFuncBack "java evn"
+which android
+checkFuncBack "android evn"
+#
+#
+cp android_sdk_components.env "${ANDROID_HOME}/android_sdk_components.env"
 (while :; do echo 'y'; sleep 3; done) | android update sdk --no-ui --all --filter "$(cat ${ANDROID_HOME}/android_sdk_components.env)"
