2391b3e74854a9733b355ed0f0ae59a25d42f0e5
sinlov <sinlovgmppt@gmail.com>
2017-07-20 23:02:14 +0800

    add maintain script for fast use

---
+++ actDocker/nginx-devops/maintain.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "This script need sudo and docker docker-compose"
+
+which docker
+docker version
+
+which docker-compose
+docker-compose version
+
+sudo pwd
+sudo docker-compose up -d
